f84bd223844662cb1adf3326deb16c4c3fe6bcf7
Prashant Gupta <antshpra@gmail.com>
2023-05-06 07:11:20 +0530

    TTL Cache

--- src/cache.js
+++ src/cache.js
@@ -1,10 +1,37 @@
+exports.ttl = function(size, ttl = 5 * 60) {
+
+  ttl = Math.max(ttl, 60);
+
+  let queue = [];
+  let map = {};
+
+  this.get = (key) => {
+    return map[key].ttl < Date.now() ? map[key].value : null;
+  };
+
+  this.put = (key, val) => {
+    map[key] = {
+      value: val,
+      expiry: Date.now() + ttl * 1000
+    };
+  };
+
+  setInterval(() => {
+    Object.entries(map).forEach(entry => {
+      if(entry[1].ttl < Date.now())
+        delete map[entry[0]];
+    });
+  }, ttl * 1000);
+
+}
+
 exports.lru = function(size) {
 
   let queue = [];
   let map = {};
 
   this.get = (key) => {
-    return map[key];
+    return map[key] || null;
   };
 
   this.put = (key, val) => {
