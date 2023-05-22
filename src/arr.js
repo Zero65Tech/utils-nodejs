e269f1bb4ca88d3b935190ab223e222715544be9
Prashant Gupta <antshpra@gmail.com>
2023-05-22 10:57:11 +0530

    Arr util

--- src/arr.js
+++ src/arr.js
@@ -1,3 +1,16 @@
+exports.toString = (arr) => {
+
+  if(!arr.length)
+    return null;
+
+  let str = '';
+  for(let item of arr)
+    str += ', ' + item;
+
+  return str.substring(2);
+  
+}
+
 exports.pushUnique = (arr, item) => {
   if(arr.indexOf(item) == -1)
     arr.push(item);
